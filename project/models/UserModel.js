a08bbf72d52cda48821806aa4cb1809261bbfdc1
Nhân Nguyễn <124347873+Nhannguyenus24@users.noreply.github.com>
2024-11-29 18:13:05 +0700

    Update UserModel.js avatar for anonymous user

--- project/models/UserModel.js
+++ project/models/UserModel.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   fullname: { type: String, required: true },
-  avatar: { type: String, required: true },
+  avatar: { type: String, default: "/image/anonymous-user.jpg" },
   quote: { type: String, default: "" },
 });
 
