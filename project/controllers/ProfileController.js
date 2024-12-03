85badbe7cf73f195f8fc3b65aa5daf9e14f220ca
tung <tranductung07012004@gmail.com>
2024-12-03 12:00:45 +0700

    Using template engine for the list-of-follower, delete the unfollowPanel because it is not required in the project

--- project/controllers/ProfileController.js
+++ project/controllers/ProfileController.js
@@ -1,6 +1,34 @@
-const loadProfile = (req, res) => {
-    res.render("Profile");
-}
+import FollowModel from '../models/FollowModel.js';
+
+const loadProfile = async (req, res) => {
+    // Dữ liệu mẫu
+    const followers = [
+        {
+            avatar: '/image/avatar_1.0.jpg',
+            username: "Spectacular",
+            fullname: "Anh Hùng",
+            status: "Đang Theo dõi"
+        },
+        {
+            avatar: '/image/avatar_1.0.jpg',
+            username: "Betagen 22",
+            fullname: "Trâm Anh",
+            status: "Đang Theo dõi"
+        },
+        {
+            avatar: '/image/avatar_1.0.jpg',
+            username: "",
+            fullname: "Thế Phiệt",
+            status: "Đang Theo dõi"
+        },
+    ];
+
+    // Tính số lượng followers
+    const followerCount = followers.length;
+
+    // Render view "Profile" với dữ liệu mẫu
+    res.render("Profile", { followers, followerCount });
+};
 
 const ProfileController = {
     loadProfile: loadProfile,
