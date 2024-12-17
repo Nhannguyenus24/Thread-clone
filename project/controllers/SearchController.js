2c2249bc27a383fcb64d76320901276bac065542
Nhân Nguyễn <124347873+Nhannguyenus24@users.noreply.github.com>
2024-12-17 15:41:25 +0700

    update follow state, view other profile, fix small error

--- project/controllers/SearchController.js
+++ project/controllers/SearchController.js
@@ -1,45 +1,49 @@
 import UserModel from '../models/UserModel.js';
 import FollowModel from '../models/FollowModel.js';
 import jwt from "jsonwebtoken";
-import { query } from 'express';
 
 const loadSearch = async (req, res) => {
-    const token = req.cookies.token;
-    if (!token)
-      return res.redirect("/login");
-    const decode = jwt.verify(
+  const token = req.cookies.token;
+  if (!token) return res.redirect("/login");
+
+  const decode = jwt.verify(
       token,
       "741017f64f83c6884e275312409462130e6b4ad31a651a1d66bf7ca08ef64ca4377e229b4aa54757dfefc268d6dbca0f075bda7a23ea913666e4a78102896f60"
-    );
+  );
+
   try {
-    const userId = decode.userId;
-    const searchQuery = req.query.q || '';
-    const users = await UserModel.find({ _id: { $ne: userId } });
+      const userId = decode.userId;
+      const searchQuery = req.query.q || '';
+      
+      const users = await UserModel.find({ _id: { $ne: userId } }).lean();
 
-    const followData = await FollowModel.findOne({ userId }).lean();
+      const followData = await FollowModel.findOne({ userId }).lean();
 
-    const followingUsernames = followData ? followData.followings.map(follow => follow.username) : [];
+      const followingUserIds = followData ? followData.followings : [];
+      const filteredProfiles = users.filter(user =>
+          user.username.toLowerCase().includes(searchQuery.toLowerCase())
+      );
 
-    const filteredProfiles = users.filter(user =>
-        user.username.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-
-    const result = filteredProfiles.map(user => {
-        const isFollowing = followingUsernames.includes(user.username);
+      const result = await Promise.all(filteredProfiles.map(async user => {
+        const userFollowData = await FollowModel.findOne({ userId: user._id }).lean();
+        const isFollowing = followingUserIds.some(followingId => followingId.equals(user._id));
+        
         return {
+            id: user._id,
             avatar: user.avatar,
             username: user.username,
             bio: user.quote || '',
-            status: isFollowing,
-            followers: followData ? followData.followers.length : 0
+            status: isFollowing, 
+            followers: userFollowData ? userFollowData.followers.length : 0
         };
-    });
-    res.render("Search", { infomations: result});
-} catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ message: "Internal server error" });
-}
+    }));
+      res.render("Search", { infomations: result });
+  } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ message: "Internal server error" });
+  }
 };
+
 
 
 const SearchController = {
